3f4ec7cfb63c0d226a535336f8c3bb38fede1346
Zijun Guo <18811434432@163.com>
2022-07-01 10:51:36 +0800

    Serial tested

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@ void setup() {
 	// put your setup code here, to run once:
 
 	/* Built in LED example */
-	// pinMode(LED_BUILTIN,OUTPUT);  //LED_BUILTIN  D4 GPIO2
+	// pinMode(LED_BUILTIN,OUTPUT);  //LED_BUILTIN  D4
 
 	/* PWM example */
-	pinMode(D8, OUTPUT);
-	analogWriteFreq(25000);
+	// pinMode(D8, OUTPUT);
+	// analogWriteFreq(25000);  // recommended freq for the Delta fan
+
+	/* Serial example */
+	Serial.pins(D10, D9);
+	Serial.begin(9600);
 }
 
 void loop() {
@@ -21,12 +25,16 @@ void loop() {
 	// delay(1000);
 
 	/* PWM example */
-	for (int i = 0; i < 255; i ++) {
-		analogWrite(D8, i);  // 255 max
-		delay(10);
-	}
-	for (int i = 255; i > 0; i --) {
-		analogWrite(D8, i);  // 255 max
-		delay(10);
-	}
+	// for (int i = 0; i < 255; i ++) {
+	// 	analogWrite(D8, i);  // 255 max
+	// 	delay(10);
+	// }
+	// for (int i = 255; i > 0; i --) {
+	// 	analogWrite(D8, i);  // 255 max
+	// 	delay(10);
+	// }
+
+	/* Serial example */
+	Serial.print("CAT\r");
+  	delay(1000);
 }
